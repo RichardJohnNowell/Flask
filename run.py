f9c45a77927ca6e9c3496d64a121338b9a232967
RichardJohnNowell <richard.j.w.nowell@outlook.com>
2023-06-12 08:51:35 +0000

    consolidated code

--- run.py
+++ run.py
@@ -1,4 +1,3 @@
-
 import os
 import json
 from flask import Flask, render_template
@@ -7,12 +6,18 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
 @app.route("/about")
 def about():
     data = []
     with open("data/company.json", "r") as json_data:
         data = json.load(json_data)
     return render_template("about.html", page_title="About", company=data)
+
 
 @app.route("/about/<member_name>")
 def about_member(member_name):
@@ -24,9 +29,11 @@ def about_member(member_name):
                 member = obj
     return render_template("member.html", member=member)
 
+
 @app.route("/contact")
 def contact():
     return render_template("contact.html", page_title="Contact")
+
 
 @app.route("/careers")
 def careers():
